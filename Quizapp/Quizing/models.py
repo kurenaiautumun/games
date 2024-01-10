b16e90ff5d31db38e992fe6bcae3922e7cdecf4a
Mohammed Owais <mohammedowais6361@gmail.com>
2024-01-10 12:38:03 +0530

    changes

--- Quizapp/Quizing/models.py
+++ Quizapp/Quizing/models.py
@@ -15,7 +15,7 @@ class Score(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=255, unique=True)
     points = models.IntegerField(default=0)
-    quizName = models.CharField(max_length=50)
+    quizName = models.CharField(max_length=50,null = True)
 
     def __str__(self):
         return f"{self.user_id}'s Score"
